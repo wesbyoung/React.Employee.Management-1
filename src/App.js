899ffd64e03af01e68384ff0eabcab7b3b292f7f
derekcodingtemple <derekh@codingtemple.com>
2020-09-21 14:09:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <main>
+          <div className="container">
+            <div className="row">
+              <div className="card-deck">
+                <div id="employees-container" className="card" style={{ width: "400px"}}>
+                  <div className="card-header">
+                    Employee
+                </div>
+                  <ul id="employees" className="list-group list-group-flush">
+                  </ul>
+                </div>
+                <div id="information-container" className="card" style={{ width: "400px"}}>
+                  <div className="card-header">
+                    Information
+                </div>
+                  <ul id="information" className="list-group list-group-flush">
+                    <li className="list-group-item"><strong>Name</strong> <span id="name" className="float-right"></span></li>
+                    <li className="list-group-item"><strong>Age</strong> <span id="age" className="float-right"></span></li>
+                    <li className="list-group-item"><strong>Occupation</strong> <span id="occupation" className="float-right"></span></li>
+                    <li className="list-group-item"><strong>Location</strong> <span id="location" className="float-right"></span></li>
+                  </ul>
+                </div>
+                <div id="skills-container" className="card" style={{ width: "400px"}}>
+                  <div className="card-header">
+                    Skills
+                </div>
+                  <ul id="skills" className="list-group list-group-flush">
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+
+        <Footer />
+      </div>
+    )
+  }
 }
-
-export default App;
